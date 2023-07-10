bf09dc2325c8ad7fb7083b86dcea8e966fddced6
foreknowledge <kyj_0129@naver.com>
2023-07-10 12:52:48 +0900

    feat: Home page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,14 @@ import { PostMeta } from '@/types/post';
 import { GetStaticProps } from 'next';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
+import styles from '@/styles/home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
+
+const metadata = {
+  title: 'Next.js Blog',
+  description: 'Next.js로 만든 블로그 입니다 :)',
+};
 
 interface Props {
   allPostsData: PostMeta[];
@@ -15,13 +21,16 @@ export default function Home({ allPostsData }: Props) {
   return (
     <>
       <Head>
-        <title>Next.js Blog</title>
-        <meta name="description" content="Next.js로 만든 블로그 입니다." />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={inter.className}>
-        <h1>Posts</h1>
+      <main className={`${styles.container} ${inter.className}`}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>{metadata.title}</h1>
+          <h3 className={styles.description}>{metadata.description}</h3>
+        </header>
         <PostList posts={allPostsData} />
       </main>
     </>
