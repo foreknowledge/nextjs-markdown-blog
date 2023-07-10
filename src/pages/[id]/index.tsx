8624df5136876f9516d99ba0c9b81c161b669526
foreknowledge <kyj_0129@naver.com>
2023-07-10 18:10:53 +0900

    feat: Styling post page header

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,8 +1,13 @@
-import PostDetail from '@/components/post-detail/PostDetail';
+import ChipList from '@/components/chip/ChipList';
 import { getAllPostIds, getPostData } from '@/lib/posts';
+import styles from '@/styles/post.module.css';
 import { PostData } from '@/types/post';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { Jua } from 'next/font/google';
 import Head from 'next/head';
+import Link from 'next/link';
+
+const jua = Jua({ subsets: ['latin'], weight: '400' });
 
 interface Props {
   postData: PostData;
@@ -16,7 +21,25 @@ export default function PostPage({ postData }: Props) {
         <meta name="description" content={postData.description} />
       </Head>
       <main>
-        <PostDetail postData={postData} />
+        <section className={styles.navBar}>
+          <button className={styles.btnGoHome}>
+            <Link href="/">← Home</Link>
+          </button>
+        </section>
+        <section className={styles.contentsWrapper}>
+          <header className={`${styles.header} ${jua.className}`}>
+            <h1 className={styles.title}>{postData.title}</h1>
+            <span className={styles.date}>{postData.date}</span>
+            {postData.tags && (
+              <div className={styles.tags}>
+                <ChipList textList={postData.tags} />
+              </div>
+            )}
+          </header>
+          <div className={styles.contents}>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          </div>
+        </section>
       </main>
     </>
   );
