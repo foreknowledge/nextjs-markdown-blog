d98c6bd27ba20802b29ab713bbb97279cda4d633
foreknowledge <kyj_0129@naver.com>
2023-07-12 23:32:42 +0900

    fix: Remove unused click handlers from chips

--- src/components/chip/Chip.tsx
+++ src/components/chip/Chip.tsx
@@ -2,15 +2,10 @@ import styles from './chip.module.css';
 
 interface Props {
   text: string;
-  onChipClick: (text: string) => void;
 }
 
-const Chip = ({ text, onChipClick }: Props) => {
-  return (
-    <div className={styles.chip} onClick={() => onChipClick(text)}>
-      {text}
-    </div>
-  );
+const Chip = ({ text }: Props) => {
+  return <div className={styles.chip}>{text}</div>;
 };
 
 export default Chip;
--- src/components/chip/ChipList.tsx
+++ src/components/chip/ChipList.tsx
@@ -3,14 +3,13 @@ import styles from './chip.module.css';
 
 interface Props {
   textList: string[];
-  onChipClick?: (text: string) => void;
 }
 
-const ChipList = ({ textList, onChipClick = () => {} }: Props) => {
+const ChipList = ({ textList }: Props) => {
   return (
     <ul className={styles.chipList}>
       {textList.map((text) => (
-        <Chip key={text} text={text} onChipClick={onChipClick} />
+        <Chip key={text} text={text} />
       ))}
     </ul>
   );
