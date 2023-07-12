bfdcb864ce9a5319ff04324cfa62cf90a8e82ab2
foreknowledge <kyj_0129@naver.com>
2023-07-12 23:47:25 +0900

    refactor: Apply metadata to post page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,6 +1,7 @@
 import ChipList from '@/components/chip/ChipList';
 import { getAllPostIds, getPostData } from '@/lib/posts';
 import styles from '@/styles/post.module.css';
+import { Metadata } from 'next';
 import { Jua } from 'next/font/google';
 import Head from 'next/head';
 import Link from 'next/link';
@@ -11,37 +12,43 @@ interface Props {
   params: { id: string };
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const postData = await getPostData(params.id);
+
+  return {
+    title: postData.title,
+    description: postData.description,
+    icons: {
+      icon: '/favicon.ico',
+    },
+  };
+}
+
 export default async function PostPage({ params }: Props) {
   const postData = await getPostData(params.id);
 
   return (
-    <>
-      <Head>
-        <title>{postData.title}</title>
-        <meta name="description" content={postData.description} />
-      </Head>
-      <main>
-        <section className={styles.navBar}>
-          <Link href="/" className={styles.btnGoHome}>
-            <span>← Home</span>
-          </Link>
-        </section>
-        <section className={styles.contentsWrapper}>
-          <header className={`${styles.header} ${jua.className}`}>
-            <h1 className={styles.title}>{postData.title}</h1>
-            <span className={styles.date}>{postData.date}</span>
-            {postData.tags && (
-              <div className={styles.tags}>
-                <ChipList textList={postData.tags} />
-              </div>
-            )}
-          </header>
-          <div className="md-contents">
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-          </div>
-        </section>
-      </main>
-    </>
+    <main>
+      <section className={styles.navBar}>
+        <Link href="/" className={styles.btnGoHome}>
+          <span>← Home</span>
+        </Link>
+      </section>
+      <section className={styles.contentsWrapper}>
+        <header className={`${styles.header} ${jua.className}`}>
+          <h1 className={styles.title}>{postData.title}</h1>
+          <span className={styles.date}>{postData.date}</span>
+          {postData.tags && (
+            <div className={styles.tags}>
+              <ChipList textList={postData.tags} />
+            </div>
+          )}
+        </header>
+        <div className="md-contents">
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </div>
+      </section>
+    </main>
   );
 }
 
